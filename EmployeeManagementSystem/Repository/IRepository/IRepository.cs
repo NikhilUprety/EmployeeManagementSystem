a836be15c84da54c9b2f41d58544b923e6bdd8bd
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-14 13:55:23 +0545

    Repository Added

---
+++ EmployeeManagementSystem/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace EmployeeManagementSystem.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+
+    {
+                //T-category
+            IEnumerable<T> GetAll();
+            T Get(Expression<Func<T,bool>> filter);
+            void Add (T entity);
+            void Remove (T entity);
+             void RemoveRange(IEnumerable<T> entity);
+
+
+
+    }   
+}
---
+++ EmployeeManagementSystem/Repository/Implementation/Repository.cs
@@ -0,0 +1,48 @@
+﻿using EmployeeManagementSystem.DataContext;
+using EmployeeManagementSystem.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace EmployeeManagementSystem.Repository.Implementation
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly EmployeeDbContext _employeeDbContext;
+        internal DbSet<T> dbset;
+        public Repository(EmployeeDbContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+            this.dbset = _employeeDbContext.Set<T>();
+            //_employeeDbContext==dbset
+        }
+        public void Add(T entity)
+        { 
+            dbset.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            dbset.Remove(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbset;
+           query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+
+            IQueryable<T> query = dbset;
+            return query.ToList();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+        dbset.RemoveRange(entity);
+        }
+    }
+}
