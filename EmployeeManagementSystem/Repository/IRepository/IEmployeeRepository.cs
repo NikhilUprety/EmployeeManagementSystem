b8a8e5e741730fefeaa1b87279aa3fe710317666
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-14 15:36:03 +0545

    repository  class added

---
+++ EmployeeManagementSystem/Repository/IRepository/IEmployeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using EmployeeManagementSystem.Models.Domain;
+using System.Linq.Expressions;
+
+namespace EmployeeManagementSystem.Repository.IRepository
+{
+    public class IEmployeeRepository : IRepository<EmployeeModel>
+    {
+        public void Add(EmployeeModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeModel Get(Expression<Func<EmployeeModel, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmployeeModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(EmployeeModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<EmployeeModel> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+ 
