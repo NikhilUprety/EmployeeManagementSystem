4dfc1ac406260707a3e218b26f421010458ef0b7
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-14 17:55:13 +0545

    employee repo implementation

---
+++ EmployeeManagementSystem/Repository/Implementation/EmployeeRepository.cs
@@ -0,0 +1,44 @@
+﻿using EmployeeManagementSystem.Models.Domain;
+using EmployeeManagementSystem.Repository.IRepository;
+using System.Linq.Expressions;
+
+namespace EmployeeManagementSystem.Repository.Implementation
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void Add(EmployeeModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeModel Get(Expression<Func<EmployeeModel, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmployeeModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(EmployeeModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<EmployeeModel> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void update(EmployeeModel obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
