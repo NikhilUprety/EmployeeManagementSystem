d06ec2ed6358581e950f2611fc51a286d967587c
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-27 17:45:55 +0545

    department controller added

---
+++ EmployeeManagementSystem/Controllers/Department.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class Department : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
