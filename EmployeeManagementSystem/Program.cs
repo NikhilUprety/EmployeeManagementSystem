3c1a102a351d18dd36c9cad88fe8b6b4c572438d
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-29 20:02:15 +0545

    program.cs ma configure gareko

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Employee}/{action=Index}/{id?}");
+   pattern: "{controller=Employee}/{action=Index}/{id?}");
 
 app.Run();
