49310ce13478997b42dc639c9d90a0e3ccfdd8be
NikhilUprety <105978476+NikhilUprety@users.noreply.github.com>
2024-01-30 16:33:06 +0545

    Admin controller added

---
+++ EmployeeManagementSystem/Areas/admin/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Areas.admin.Controllers
+{
+    [Authorize]
+
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
